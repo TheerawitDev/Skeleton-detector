daf01591377b904f1b8caa01f19cd775bca2724c
Theerawit Waithayawan <pm.theerawit@gmail.com>
2025-10-04 21:43:46 +0700

    Smaller model, more fps

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import cv2
 from ultralytics import YOLO
 
-models = YOLO("yolo11n-pose.pt")
-cap = cv2.VidioCapture(0, cv.CAP_DSHOW)
+print("program stating...")
+model = YOLO("yolo11n-pose.pt")
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
     ok, frame = cap.read()
     if not ok:
         break
+    results = model.predict(frame, imgsz=512, conf=0.5, iou=0.5, verbose=False)
+
+    annotated = results[0].plot() 
+    cv2.imshow("ESC to quit", annotated)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
